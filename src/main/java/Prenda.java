9152b1750c4507a0ab22839fbcb6e5c3ee261b7a
Yamila Aylén Soto <yamilaylensoto@gmail.com>
2020-04-15 17:00:28 -0300

    Resolución problema Color Secundario

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -4,8 +4,19 @@ public class Prenda {
 	private String categoria;
 	private String colorPrimario;
 	private String colorSecundario;
+	
+//-------------------------------------------------------------------------------------------------------------Constructor de prenda SIN colorSecundario
+	public Prenda(Tipo tipo, String colorPrimario, String material, String categoria){
+		this.tipo = tipo; 
+		this.colorPrimario = colorPrimario; 
+		this.material = material; 
+		this.categoria = categoria;
+			if(!tipo.esDeCategoria(categoria)){
+				throw new ErrorCategoriaNoCondiceTipo();
+			}
+		}
 
-// sin colorSecundario?
+//-------------------------------------------------------------------------------------------------------------Constructor de prenda CON colorSecundario
 	public Prenda(Tipo tipo, String colorPrimario, String material, String categoria, String colorSecundario){
 		this.tipo = tipo; 
 		this.colorPrimario = colorPrimario; 
